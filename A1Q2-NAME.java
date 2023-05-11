3206ccad2dd8f4e5d6ca518538d651d097909b02
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-05-11 10:29:56 +0530

    Create A1Q2-NAME.java

---
+++ A1Q2-NAME.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class a1q1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String name = in.nextLine();
+
+        System.out.println("Hello !" + " " + " Mr. " + name);
+        }
+
+    }
+
